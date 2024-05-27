252699868f277016bce87c2e0c9e3c390b7a3757
nsjames <DENIED>
2024-05-27 13:22:21 +0100

    metatags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,18 @@
     import "$lib/app.css";
 </script>
 
+<svelte:head>
+    <title>EOS MSIG Viewer</title>
+    <meta name="description" content="A viewer for EOS multisig proposals" />
+    <meta property="og:image" content="https://msigviewer.netlify.app/ogimage.png">
+    <meta property="og:locale" content="en" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="EOS MSIG Viewer" />
+    <meta property="og:description" content="A viewer for EOS multisig proposals" />
+    <meta property="og:url" content="https://msigviewer.netlify.app" />
+    <meta property="og:site_name" content="EOS MSIG Viewer" />
+</svelte:head>
+
 <slot />
