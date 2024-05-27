80b7a68ab050c75b730898e2c0c02d1a4cac947b
nsjames <DENIED>
2024-05-27 13:14:57 +0100

    load faster and change url on loading

--- src/routes/[...params]/+page.svelte
+++ src/routes/[...params]/+page.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
     import {getActions} from "$lib";
     import {NETWORKS} from "$lib/networks";
+    import {onMount} from "svelte";
 
     export let data;
 
-    $: actions = data.actions;
+    let actions:any = null;
 
-    let account:string = "eosnationftw";
-    let proposal:string = "main.test";
-    let network:string = NETWORKS.mainnet;
+
+    let account:string = data.account || "";
+    let proposal:string = data.proposal || "";
+    let network:string = data.network || NETWORKS.mainnet;
     let loading:boolean = false;
+
+    const updateUrl = () => {
+        if(!account || !proposal){
+            window.history.pushState({}, '', '/');
+        } else {
+            const networkKey = Object.keys(NETWORKS).find(key => NETWORKS[key] === network);
+            const url = `/${networkKey}/${account}/${proposal}`;
+            window.history.pushState({}, '', url);
+        }
+    }
 
     const findProposal = async () => {
         loading = true;
+        updateUrl();
         const res = await getActions(fetch, account, proposal, network).catch(err => {
             alert(err);
             return null;
@@ -25,7 +38,7 @@
         }
 
         if(!res) return;
-        data.actions = res;
+        actions = res;
     }
 
     const readableValue = (value:any) => {
@@ -47,9 +60,13 @@
         a.download = `${action.data.account}.${suffix}`;
         a.click();
     }
+
+    onMount(() => {
+        if(account && proposal) {
+            findProposal();
+        }
+    })
 </script>
-
-
 
 
 <section class="p-2 bg-zinc-950 min-h-screen">
@@ -82,48 +99,69 @@
     </section>
 
     <section class="flex flex-col gap-2 mx-auto max-w-3xl pt-10">
-        {#if actions}
-            {#each actions as action,index}
-                <section class="bg-white text-black rounded-lg p-4">
-                    <section class="flex items-center gap-2">
-                        <figure class="p-1 px-2 bg-zinc-600 text-white rounded text-xs mt-1">
-                            #{index+1}
-                        </figure>
-                        <figure class="text-xl"><span class="font-bold">{action.account} :: <u>{action.name}</u></span></figure>
-                    </section>
+        {#if loading}
+            <div class="skeleton w-full h-[100px] mb-4 rounded"></div>
+        {:else}
+            {#if actions}
+                {#each actions as action,index}
+                    <section class="bg-white text-black rounded-lg p-4">
+                        <section class="flex items-center gap-2">
+                            <figure class="p-1 px-2 bg-zinc-600 text-white rounded text-xs mt-1">
+                                #{index+1}
+                            </figure>
+                            <figure class="text-xl"><span class="font-bold">{action.account} :: <u>{action.name}</u></span></figure>
+                        </section>
 
-                    <section class="bg-gray-50 border border-zinc-200 rounded-lg p-4 mt-2 text-xs overflow-x-scroll">
-                        {#if !Object.keys(action.data).length}
-                            <figure class="opacity-30">No parameters for this action</figure>
-                        {:else}
-                            {#each Object.keys(action.data) as key}
-                                {#if key !== '_rawCodeOrAbi'}
-                                    <figure>
-                                        {key}: <span class="font-bold whitespace-pre break-words">{readableValue(action.data[key])}</span>
-                                    </figure>
-                                {/if}
-                            {/each}
+                        <section class="bg-gray-50 border border-zinc-200 rounded-lg p-4 mt-2 text-xs overflow-x-scroll">
+                            {#if !Object.keys(action.data).length}
+                                <figure class="opacity-30">No parameters for this action</figure>
+                            {:else}
+                                {#each Object.keys(action.data) as key}
+                                    {#if key !== '_rawCodeOrAbi'}
+                                        <figure>
+                                            {key}: <span class="font-bold whitespace-pre break-words">{readableValue(action.data[key])}</span>
+                                        </figure>
+                                    {/if}
+                                {/each}
+                            {/if}
+                        </section>
+
+                        {#if action.account === 'eosio' && action.name === 'setcode'}
+                            <button on:click={() => downloadFile(action, 'wasm')} class="bg-blue-600 hover:bg-blue-500 text-white text-xs p-1.5 px-3 font-bold rounded mt-2">
+                                Download WASM
+                            </button>
+                        {/if}
+
+                        {#if action.account === 'eosio' && action.name === 'setabi'}
+                            <button on:click={() => downloadFile(action, 'abi')} class="bg-blue-600 hover:bg-blue-500 text-white text-xs p-1.5 px-3 font-bold rounded mt-2">
+                                Download ABI
+                            </button>
                         {/if}
                     </section>
-
-                    {#if action.account === 'eosio' && action.name === 'setcode'}
-                        <button on:click={() => downloadFile(action, 'wasm')} class="bg-blue-600 hover:bg-blue-500 text-white text-xs p-1.5 px-3 font-bold rounded mt-2">
-                            Download WASM
-                        </button>
-                    {/if}
-
-                    {#if action.account === 'eosio' && action.name === 'setabi'}
-                        <button on:click={() => downloadFile(action, 'abi')} class="bg-blue-600 hover:bg-blue-500 text-white text-xs p-1.5 px-3 font-bold rounded mt-2">
-                            Download ABI
-                        </button>
-                    {/if}
+                {/each}
+            {:else}
+                <section class="bg-white rounded-lg text-black text-center p-4 py-8">
+                    <figure>No proposal found</figure>
                 </section>
-            {/each}
-        {:else}
-            <section class="bg-white rounded-lg text-black text-center p-4 py-8">
-                <figure>No proposal found</figure>
-            </section>
+            {/if}
         {/if}
 
     </section>
 </section>
+
+<style>
+    @keyframes shine {
+        0% {
+            background-position: -200px 0;
+        }
+        100% {
+            background-position: 200px 0;
+        }
+    }
+
+    .skeleton {
+        background: linear-gradient(40deg, #1f1f1f 25%, #313131 50%, #212121 75%);
+        background-size: 400px 100%;
+        animation: shine 2s infinite;
+    }
+</style>
--- src/routes/[...params]/+page.ts
+++ src/routes/[...params]/+page.ts
@@ -9,12 +9,12 @@ export const load = async ({ fetch, url }) => {
     network = network ? NETWORKS[network.toLowerCase()] : null;
     if(!network) network = NETWORKS.mainnet;
 
-    if(!account) account = "eosnationftw";
-    if(!proposal) proposal = "main.test";
-
-    const actions = await getActions(fetch, account, proposal, network);
+    // if(!account) account = "eosnationftw";
+    // if(!proposal) proposal = "main.test";
+    //
+    // const actions = await getActions(fetch, account, proposal, network);
     return {
-        actions
+        network, account, proposal
     };
 
 };
