b8ada8f90c8395eff50bfafae172d8c6a88e9c60
nsjames <DENIED>
2024-06-14 22:13:07 +0100

    fix jungle endpoint

--- src/lib/networks.ts
+++ src/lib/networks.ts
@@ -1,5 +1,5 @@
 export const NETWORKS:any = {
     kylin: 'https://kylin.api.eosnation.io',
     mainnet: 'https://eos.greymass.com',
-    jungle: 'https://jungle4.greymass.io',
+    jungle: 'https://jungle4.greymass.com',
 }
